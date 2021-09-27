d7ced949366277df970221dc8bc878022c4bbd77
yegorkravchenko <kraver.kravchenko@gmail.com>
2021-09-27 22:31:10 +0200

    Add NotFound page to router

---
+++ src/pages/NotFound/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <>
+      <p>The requested page was not found.</p>
+    </>
+  );
+}
+
+export default NotFound;
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import About from '../pages/About';
 import Home from '../pages/Home';
+import NotFound from '../pages/NotFound';
 
 export const routes = [
   { path: '/', component: Home, exact: true },
   { path: '/about', component: About, exact: true },
+  { path: '', component: NotFound, exact: false },
 ];
